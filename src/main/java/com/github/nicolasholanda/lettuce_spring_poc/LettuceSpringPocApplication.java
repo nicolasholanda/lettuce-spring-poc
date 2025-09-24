cb47641b4a3cf35f84e78230f15cd386b1d79419
Nicolas Holanda <nikolas.nhm14@gmail.com>
2025-09-24 09:53:00 -0300

    Initial commit

---
+++ src/main/java/com/github/nicolasholanda/lettuce_spring_poc/LettuceSpringPocApplication.java
@@ -0,0 +1,13 @@
+package com.github.nicolasholanda.lettuce_spring_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LettuceSpringPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LettuceSpringPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/nicolasholanda/lettuce_spring_poc/LettuceSpringPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nicolasholanda.lettuce_spring_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LettuceSpringPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
